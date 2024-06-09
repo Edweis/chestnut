26470b04603e78a3db5af806badff23ce1669245
François Rullière <kapochamo@gmail.com>
2024-06-09 20:26:55 +0200

    add backup

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import Koa from 'koa';
 import route from 'koa-route';
 import websockify from 'koa-websocket';
-import fs from 'fs'
+import fs from 'fs/promises'
 const app = websockify(new Koa());
 
 class BetterMap extends Map {
@@ -15,22 +15,38 @@ class BetterMap extends Map {
 
 const rooms = new BetterMap()
 
-app.ws.use(route.all('/join/:roomId/:userId', function (ctx, roomId, userId) {
+app.use(async (ctx, next) => {
+  try {
+    return await next()
+  } catch (err) {
+    console.log(err)
+    throw err
+  }
+})
+
+app.ws.use(route.all('/join/:roomId/:userId', async (ctx, roomId, userId) => {
   const room = rooms.getOrCreate(roomId, new Map());
   room.set(userId, ctx.websocket) // joined the room 
+  const chatPath = './chats/' + roomId + '.txt'
 
   // Hello everyone
+  const history = await fs.readFile(chatPath).catch(() => '(no history)').then(r => r.toString())
+  history.split('\n').forEach(l => ctx.websocket.send(l))
   room.forEach((ws) => ws.send(`# ${userId} has joined the chat`));
 
   // Message sent
   ctx.websocket.on('message', function (message) {
     const formated = `[${new Date().toISOString()}|${userId}]${message}`
-    fs.appendFileSync('./chats/' + roomId + '.txt', formated+'\n')
+    fs.appendFile(chatPath, formated + '\n')
     room.forEach((ws, participantId) => {
       if (participantId === userId) return // not sending the message to oneself !
       ws.send(formated)
     })
   });
+
+  ctx.websocket.on('error', err => console.warn('FAILED', err))
+  ctx.websocket.on('close', err => console.warn('CLOSED', err))
 }));
+
 
 app.listen(3000);
