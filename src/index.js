c660266cd6a98783ce6ce61f8c2dd25064b0b23e
François Rullière <kapochamo@gmail.com>
2024-06-10 06:52:39 +0200

    minimum chat app

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import Koa from 'koa';
 import route from 'koa-route';
 import websockify from 'koa-websocket';
 import fs from 'fs/promises'
+import hbs from 'handlebars'
+import dayjs from 'dayjs'
+
 const app = websockify(new Koa());
 
 class BetterMap extends Map {
@@ -13,8 +16,8 @@ class BetterMap extends Map {
   }
 }
 
-const rooms = new BetterMap()
 
+// error handling
 app.use(async (ctx, next) => {
   try {
     return await next()
@@ -24,6 +27,26 @@ app.use(async (ctx, next) => {
   }
 })
 
+// helpers
+const CLEAR_CHAT = `<div id="notifications"></div>`
+const format = (message) => {
+  const [, date, name, content] = message.match(/^\[(.*?)\|(.*?)\](.*)$/) || [, , , message]
+  if (!date) return `<div id="notifications" hx-swap-oob="beforeend">
+                      <div>${content}</div>
+                    </div>`
+  return `<div id="notifications" hx-swap-oob="beforeend">
+            <div>${dayjs(date).format('HH:mm:ss')} - ${name} : ${content}</div>
+          </div>`
+}
+
+// frontend
+const template = await fs.readFile('./src/main.hbs')
+const mainPage = hbs.compile(template.toString())
+app.use(route.get('/', async (ctx) => {
+  ctx.body = mainPage()
+}))
+
+const rooms = new BetterMap()
 app.ws.use(route.all('/join/:roomId/:userId', async (ctx, roomId, userId) => {
   const room = rooms.getOrCreate(roomId, new Map());
   room.set(userId, ctx.websocket) // joined the room 
@@ -31,21 +54,26 @@ app.ws.use(route.all('/join/:roomId/:userId', async (ctx, roomId, userId) => {
 
   // Hello everyone
   const history = await fs.readFile(chatPath).catch(() => '(no history)').then(r => r.toString())
-  history.split('\n').forEach(l => ctx.websocket.send(l))
-  room.forEach((ws) => ws.send(`# ${userId} has joined the chat`));
+  ctx.websocket.send(CLEAR_CHAT)
+  history.split('\n').forEach(l => ctx.websocket.send(format(l)))
+
+  //Welcome message
+  room.forEach((ws) => ws.send(format(`[|]# ${userId} has joined the chat`)));
 
   // Message sent
   ctx.websocket.on('message', function (message) {
-    const formated = `[${new Date().toISOString()}|${userId}]${message}`
+    console.log(message)
+    const content = JSON.parse(message).message
+    const formated = `[${new Date().toISOString()}|${userId}]${content}`
     fs.appendFile(chatPath, formated + '\n')
-    room.forEach((ws, participantId) => {
-      if (participantId === userId) return // not sending the message to oneself !
-      ws.send(formated)
-    })
+    room.forEach((ws) => ws.send(format(formated)))
+    ctx.websocket.send('<form id="form" ws-send > <input autofocus name="message"> </form>')
   });
 
-  ctx.websocket.on('error', err => console.warn('FAILED', err))
-  ctx.websocket.on('close', err => console.warn('CLOSED', err))
+  ctx.websocket.on('close', () => {
+    room.delete(userId); 
+    room.forEach((ws) => ws.send(format(`[|]# ${userId} left`)))
+  })
 }));
 
 
